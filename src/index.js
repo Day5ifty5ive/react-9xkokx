0f3a3fb8b3785a27658e82d79a6cb1f603d3d326
Day5ifty5ive <dayfiftyfive@gmail.com>
2021-07-01 11:42:29 +0200

    1 file modified

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
-import App_2 from "./App-2";
+import App_2 from "./App_2";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 ReactDOM.render(<App_2 />, document.getElementById("root"));
