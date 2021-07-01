b55bc5fb20c9a5e51ef8548b397d233331d9a0cd
Day5ifty5ive <dayfiftyfive@gmail.com>
2021-07-01 10:48:03 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+    <form class="scr1" method="post" action="" name="signup-form">
+<div class="form-element">
+<label>•</label>
+</div>
+<div class="form-element">
+<label>•</label>
+</div>
+<div class="form-element">
+<label>•</label>
+</div>
+<button type="submit" name="register" value="register">Rifiuta</button>
+<button type="submit" name="register" value="register">Accetta</button>
+</form>
+    </div>
+  );
+}
