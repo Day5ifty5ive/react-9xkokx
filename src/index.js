04c972153b6cbf35ff01e7c3d85fe4e3f410b0ab
Day5ifty5ive <dayfiftyfive@gmail.com>
2021-07-02 10:20:57 +0200

    2 files modified

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App_1';
+import App_1 from './App_1';
 import App_2 from './App_2';
 
-ReactDOM.render(<App_2 />, document.getElementById('root'));
+ReactDOM.render(<App_1 />, document.getElementById('root'));
 
-
-/*
 function nextWindow() {
   // alert("!")
   ReactDOM.render(<App_2 />, document.getElementById("root"));
 }
-*/
